31e6299a2b8a64a61acf0b0f44f956216359b220
Kosta <dimitrovskikosta2@gmail.com>
2022-05-25 15:59:33 +0200

    "add test"

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+    public List<String> generateString(String ... elems){
+        return new ArrayList<>(Arrays.asList(elems));
+    }
+    @Test
+    public void everyStatement(){
+
+    }
+}
